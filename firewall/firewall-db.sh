eaeae591857e2c3bc89d58ebe2056e3faa69aa4e
hipzi <hipzi@localhost.localdomain>
2021-07-28 22:34:28 -0400

    tambah firewall

---
+++ firewall/firewall-db.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+iptables -F
+
+# allow SSH
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# allow HTTP(S)
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# allow connections to and from wordpress and DB
+iptables -A INPUT -s 192.168.1.15 -j ACCEPT
+iptables -A INPUT -s 192.168.1.16 -j ACCEPT
+
+# allow DB Master & Slave
+iptables -A INPUT -s 192.168.1.17 -j ACCEPT
+iptables -A INPUT -s 192.168.1.18 -j ACCEPT
+
+# allow mysql-exporter
+iptables -A INPUT -p tcp --dport 9104 -j ACCEPT
+
+# allow localhost
+iptables -A INPUT -s 127.0.0.1 -j ACCEPT
+
+# allow node-exporter
+iptables -A INPUT -p tcp --dport 9100 -j ACCEPT
+
+# Drop all and Logging
+iptables -N LOGGING
+iptables -A INPUT -j LOGGING
+iptables -A LOGGING -m limit --limit 2/min -j LOG --log-prefix "IPTables-Dropped: " --log-level 4
+iptables -A LOGGING -j DROP
---
+++ firewall/firewall-haproxy-monitoring.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+iptables -F
+
+# allow SSH
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# allow HTTP(S)
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+# allow grafana 
+iptables -A INPUT -p tcp --dport 3000 -j ACCEPT
+
+# allow prometheus 
+iptables -A INPUT -p tcp --dport 9090 -j ACCEPT
+
+# allow haproxy statistic 
+iptables -A INPUT -p tcp --dport 8404 -j ACCEPT
+
+# allow node-exporter
+iptables -A INPUT -p tcp --dport 9100 -j ACCEPT
+
+# Drop all and Logging
+iptables -N LOGGING
+iptables -A INPUT -j LOGGING
+iptables -A LOGGING -m limit --limit 2/min -j LOG --log-prefix "IPTables-Dropped: " --log-level 4
+iptables -A LOGGING -j DROP
