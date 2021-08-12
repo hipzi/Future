11f48ead087ddbbb4cbb97343a00d50a8248ee62
hipzi <zahramillah582@gmail.com>
2021-08-13 01:04:07 +0700

    add builder packer for ansible server

--- packer/create_vm.sh
+++ packer/create_vm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-vmName=("ansiblecontroller" "haproxy-monitoring" "wordpress-1" "wordpress-2" "nfs" "mariadb-master" "mariadb-slave")
-ip=("192.168.1.10" "192.168.1.11" "192.168.1.12" "192.168.1.13" "192.168.1.14" "192.168.1.15" "192.168.1.16")
+vmName=("haproxy-monitoring" "wordpress-1" "wordpress-2" "nfs" "mariadb-master" "mariadb-slave")
+ip=("192.168.1.11" "192.168.1.12" "192.168.1.13" "192.168.1.14" "192.168.1.15" "192.168.1.16")
+vmAnsible="ansiblecontroller"
+ipAnsible="192.168.1.10"
 i=0
 
 for value in "${vmName[@]}"
@@ -10,3 +12,6 @@ do
     packer build -var "vm_name=$value" -var "output_directory=$value" centos7.json 
     ((i=i+1))
 done
+
+sed -i "s/ip=${ip[5]}/ip=$ipAnsible/g" ~/packer/kickstart/ks.cfg
+packer build -var "vm_name=$vmAnsible" -var "output_directory=$vmAnsible" centos7-ansible.json
