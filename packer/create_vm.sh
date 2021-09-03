f8c16afab2a64009d23fe0115902a05a29e8c05b
hipzi <zahramillah582@gmail.com>
2021-09-03 09:40:26 +0700

    update packer

--- packer/create_vm.sh
+++ packer/create_vm.sh
@@ -10,8 +10,10 @@ for value in "${vmName[@]}"
 do 
     sed -i "s/ip=${ip[i-1]}/ip=${ip[i]}/g" ~/packer/kickstart/ks.cfg
     packer build -var "vm_name=$value" -var "output_directory=$value" centos7.json 
+    vboxmanage startvm "$value" --type headless
     ((i=i+1))
 done
 
 sed -i "s/ip=${ip[5]}/ip=$ipAnsible/g" ~/packer/kickstart/ks.cfg
 packer build -var "vm_name=$vmAnsible" -var "output_directory=$vmAnsible" centos7-ansible.json
+vboxmanage startvm "$vmAnsible" --type headless
